34fc5cc9337d56316b3ccb3b3fc5b46e1c924ac3
jialin_sun <86958975+jialin-sun2019@users.noreply.github.com>
2021-07-05 20:24:52 +0800

    Create learn_git.py

---
+++ learn_git.py
@@ -0,0 +1,7 @@
+git config --global user.name "your_name"
+git config --global user.emain your_email@.com
+
+git init
+git add .
+git commint -m "你的提交信息"
+git push   // put your username put your password
